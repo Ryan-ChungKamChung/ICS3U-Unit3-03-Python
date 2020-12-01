38d63daffc211e7314bbc47e65ca3dde38e9a6cb
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2020-12-01 16:20:40 +0000

    added magic_number.py

---
+++ magic_number.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in November 2020
+# RNG number guessing game
+
+import random
+
+
+def main():
+    # This function sees if the user inputed the magic number
+
+    print("Guess the magic number (0-9)!")
+
+    # Input
+    magic_number = int(input("Please enter your guess: "))
+
+    # Process
+    random_number = random.randint(0, 9)
+
+    if magic_number == random_number:
+        # Output
+        print("Nice! Your answer is right!")
+
+    elif magic_number != random_number:
+        # Output
+        print("Oops! Your answer is wrong!")
+        print("The correct answer was: {}".format(random_number))
+
+
+if __name__ == "__main__":
+    main()
